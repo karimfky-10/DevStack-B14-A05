26765cdfcdda9d67459de1510489ed6e0a35d5a9
karimfky-10 <karimfky@gmail.com>
2026-09-25 20:58:37 +0600

    mobile app

--- src/Component/button.tsx
+++ src/Component/button.tsx
@@ -1,13 +1,13 @@
 interface IType{
     Add: boolean,
-    handleClick: ()=> any,
-    id:any
+    handleClick: ( id:number)=> any,
+    id:number
 }
 export default function Button1({Add, handleClick, id}:IType) {
     return (
         <div>
              <button
-                onClick={()=> handleClick(id.id)}
+                onClick={()=> handleClick(id)}
                 className={Add? `text-white bg-black w-full h-8 rounded-sm` 
                 :`text-black-500 bg-olive-100 border border-olive-300 w-full h-8 rounded-sm`}
                 >{Add ? 'Add to Stack' : 'Aded'}
